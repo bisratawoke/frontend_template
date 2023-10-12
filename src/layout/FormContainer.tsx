2f8523a11e9a2ffa211bb32d3ac20c5623065667
bisratawoke <bisrat@360ground.com>
2023-10-12 19:31:27 +0300

    ready for test

--- src/layout/FormContainer.tsx
+++ src/layout/FormContainer.tsx
@@ -6,7 +6,7 @@ export default function FormContainer({
   title: string;
 }) {
   return (
-    <div className="col-start-5 col-end-9">
+    <div className="md:col-start-5 md:col-end-9 col-span-1">
       <div className="flex flex-col gap-3 bg-white p-5">
         <div className="flex flex-col gap-2">
           <span className="text-lg font-bold">{title}</span>
